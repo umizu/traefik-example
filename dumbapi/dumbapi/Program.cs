26fa7c5f9024ecdb384e3e8a0d16504cc79c3627
umizu <ninjaz.gg@outlook.com>
2022-12-21 16:04:32 -0500

    migrate

---
+++ dumbapi/dumbapi/Program.cs
@@ -0,0 +1,19 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World! from dummy api #1");
+app.Map("/hey", () => "Hey there! from dummy api #1");
+app.MapHealthChecks("/health");
+
+app.Run();
